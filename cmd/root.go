84258c78f21bfe415b596315890966d1fe68fa2c
Unknown15082 <trangiahuy15082006@gmail.com>
2022-09-24 13:14:09 +0700

    Root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,18 @@
 package cmd
 
 import (
-	_ "github.com/spf13/cobra"
+	"os"
+
+	"github.com/spf13/cobra"
 )
+
+var rootCommand = &cobra.Command{
+	Use: "httpstatus",
+	Short: "Display HTTP status codes",
+}
+
+func Execute() {
+	if err := rootCommand.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
